578f1434b6a67614253a07a0a41593b4305fcf25
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-08-09 14:21:14 +0800

    Home Page Add Minor Things

--- src/components/FeaturedPets.jsx
+++ src/components/FeaturedPets.jsx
@@ -1,9 +1,18 @@
 function FeaturedPets() {
     return (
         <>
-         <h1>
-            FEATURED PETS AT LEAST 2-4 PETS
-         </h1>
+         <div className="pet-card">
+                <img src="pet1.jpg" alt="Featured Pet 1" />
+                <h3>Pet Name</h3>
+                <p>Breed: Breed Name</p>
+                <p>Age: X years</p>
+            </div>
+            <div className="pet-card">
+                <img src="pet2.jpg" alt="Featured Pet 2" />
+                <h3>Pet Name</h3>
+                <p>Breed: Breed Name</p>
+                <p>Age: X years</p>
+            </div> 
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,18 @@
 import { Link } from 'react-router-dom';
 import '../style/home.css';
 import '../style/main.css';
+import FeaturedPets from '../components/FeaturedPets';
 
 function Home() {
     return (
         <>
     <div className='body'>
-        <img src="https://stowvets.co.uk/wp-content/uploads/sites/178/2020/10/MiPet-Cover-Pet-Insurance-Banner-Oct20-1.jpg" className="cover-photo" />
-        <h1>FurFam Finder</h1>
+        <img src="https://stowvets.co.uk/wp-content/uploads/sites/178/2020/10/MiPet-Cover-Pet-Insurance-Banner-Oct20-1.jpg" className="cover-photo" style={{opacity: '80%'}} />
+        <h1 className='title'>FurFam Finder</h1>
         <h4>Discover your perfect FurFam today! 🐾🏠 Adopt, love, cherish.</h4>
         <section className="featured-pets">
             <h2>Featured Pets for Adoption</h2>
-            <div className="pet-card">
-                <img src="pet1.jpg" alt="Featured Pet 1" />
-                <h3>Pet Name</h3>
-                <p>Breed: Breed Name</p>
-                <p>Age: X years</p>
-            </div>
-            <div className="pet-card">
-                <img src="pet2.jpg" alt="Featured Pet 2" />
-                <h3>Pet Name</h3>
-                <p>Breed: Breed Name</p>
-                <p>Age: X years</p>
-            </div>
+            <FeaturedPets />
         </section>
 
       <section className="about-us">
