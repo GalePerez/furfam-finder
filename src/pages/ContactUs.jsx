c763a20d766388748ba0495ad16f004c08b9ddcb
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-08-10 01:33:02 +0800

    Styled Contact page Progress: 80%

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import "../style/contact-us.css"
 
 function ContactUs() {
     const [name, setName] = useState('');
@@ -16,32 +17,37 @@ function ContactUs() {
       <div className='body'>
         <h2>Contact Us</h2>
         <p>Have questions or feedback? We'd love to hear from you!</p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
-          <button type="submit">Submit</button>
-        </form>
+        <section className='form'>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <br/>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <br/>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            />
+            <br/>
+            <button type="submit">Submit</button>
+          </form>
+        </section>
     </div>
         </>
     )
