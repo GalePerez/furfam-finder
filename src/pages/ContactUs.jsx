d7bc3ec634807c5d051acefa22106c77f0544536
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-08-10 21:06:04 +0800

    Styled Contact Page Complete

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -19,33 +19,40 @@ function ContactUs() {
         <p>Have questions or feedback? We'd love to hear from you!</p>
         <section className='form'>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name:</label>
+            <label htmlFor="name" className='form-label'>Name:</label>
             <input
               type="text"
               id="name"
+              className='form-control'
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
             />
             <br/>
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="email" className='form-label'>Email:</label>
             <input
               type="email"
               id="email"
+              className='form-control'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
+              placeholder='name@example.com'
             />
             <br/>
-            <label htmlFor="message">Message:</label>
+            <label htmlFor="message" className='form-label'>Message:</label>
             <textarea
               id="message"
+              className='form-control'
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               required
+              rows={3}
             />
             <br/>
-            <button type="submit">Submit</button>
+            <div className='button'>
+              <button type="submit">Submit</button>
+            </div>
           </form>
         </section>
     </div>
