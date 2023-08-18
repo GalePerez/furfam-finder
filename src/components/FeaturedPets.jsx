b2862df6e06ba7163509ed71f540b56cf6f38455
Gisel Jean Catbagan <gsljn112198@gmail.com>
2023-08-18 18:39:49 +0800

    Added fetching data in featured pets

--- src/components/FeaturedPets.jsx
+++ src/components/FeaturedPets.jsx
@@ -1,22 +1,35 @@
+import { useEffect, useState } from "react"
 import "../style/featuredpets.css"
+import axios from "axios"
 
 function FeaturedPets() {
+    const [featuredPets, setfeaturedPets] = useState({})
+
+    const fetchfeaturedPets = async () => {
+        const res = await axios('http://localhost:8000/api/v1/pets')
+        const pet = res.data.data
+        console.log(res)
+        setfeaturedPets(pet.slice(0, 4))
+        console.log(pet.slice(0, 4))
+    }
+
+    useEffect(() => {
+        fetchfeaturedPets()
+    }, [])
     return (
         <> 
-            <div className="row">
-                    <div className="card pet-card col-3 p-0 rounded-5 my-2" style={{width: "20rem"}}>
-                        <img src="https://i.etsystatic.com/22272444/r/il/c268c8/2175449994/il_fullxfull.2175449994_39b8.jpg" className="card-img-top rounded-5" alt="..." />
+            <div className="featured-pets-container">
+            {featuredPets.map(pet => (
+                <div key={pet.id} className="card" style={{ width: "18rem" }}>
+                    <img src={pet.image} className="card-img-top" alt={pet.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">{pet.species}</h5>
+                        <p className="card-text">Name: {pet.name}</p>
+                        <p className="card-text">Breed: {pet.breed}</p>
                     </div>
-                    <div className="card pet-card col-3 p-0 rounded-5 my-2" style={{width: "20rem"}}>
-                        <img src="https://i.etsystatic.com/22272444/r/il/c268c8/2175449994/il_fullxfull.2175449994_39b8.jpg" className="card-img-top rounded-5" alt="..." />
-                    </div>
-                    <div className="card pet-card col-3 p-0 rounded-5 my-2" style={{width: "20rem"}}>
-                        <img src="https://i.etsystatic.com/22272444/r/il/c268c8/2175449994/il_fullxfull.2175449994_39b8.jpg" className="card-img-top rounded-5" alt="..." />
-                    </div>
-                    <div className="card pet-card col-3 p-0 rounded-5 my-2" style={{width: "20rem"}}>
-                        <img src="https://i.etsystatic.com/22272444/r/il/c268c8/2175449994/il_fullxfull.2175449994_39b8.jpg" className="card-img-top rounded-5" alt="..." />
-                    </div>
-            </div>
+                </div>
+            ))}
+        </div>
              
         </>
     )
