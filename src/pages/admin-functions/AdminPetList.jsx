d598d97eb0e3cefbbc7968fb70de78e8160c9c62
Gisel Jean Catbagan <gsljn112198@gmail.com>
2023-08-22 14:14:50 +0800

    Added Post method in Admin

--- src/pages/admin-functions/AdminPetList.jsx
+++ src/pages/admin-functions/AdminPetList.jsx
@@ -3,14 +3,28 @@ import AdminContainer from '../../components/AdminContainer';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addPet } from '../../store/addPetActions';
+import { Form, Field, ErrorMessage, Formik } from 'formik';
+import * as Yup from 'yup';
 
+
+const validationSchema = Yup.object({
+  species: Yup.string().required('Species is required'),
+  name: Yup.string().required('Name is required'),
+  breed: Yup.string().required('Breed is required'),
+  birthday: Yup.string(),
+  gender: Yup.string(),
+  size: Yup.string(),
+  description: Yup.string(),
+  availability_status: Yup.string(),
+  image: Yup.mixed().required('Image is required'),
+});
 
 function AdminPetList() {
   const dispatch = useDispatch()
   const [petData, setPetData] = useState([])
   const [newPet, setNewPet] = useState({
     species: '',
-    image: '',
+    image: 'null',
     name: '',
     breed: '',
     birthday: '',
@@ -31,31 +45,54 @@ function AdminPetList() {
   };
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setNewPet((prevPet) => ({ ...prevPet, [name]: value }));
+    const { name, value, files } = event.target;
+    if (name === 'image') {
+      setNewPet((prevPet) => ({ ...prevPet, image: files[0] }));
+    } else {
+      setNewPet((prevPet) => ({ ...prevPet, [name]: value }));
+    }
   };
 
-  const handleAddPet = async () =>  {
-    const { data } =
-      await axios.post('http://localhost:8000/api/v1/pets', {
-        species: 'newPet.species',
-        name: 'newPet.name',
-        breed: 'newPet.breed',
-        birthday: 'newPet.birthday',
-        gender: 'newPet.gender',
-        size: 'newPet.size',
-        description: 'newPet.description',
-        availability_status: 'newPet.availability_status',
-        image: 'newPet.image',
+  const handleAddPet = async (newPet) => {
+    try {
+      // const formData = new FormData();
+      // for (const key in newPet) {
+      //   formData.append(key, newPet[key]);
+      // }
+      const res = await axios.post('http://localhost:8000/api/v1/pets', {
+        species: newPet.species,
+        name: newPet.name,
+        breed: newPet.breed,
+        birthday: newPet.birthday,
+        gender: newPet.gender,
+        size: newPet.size,
+        description: newPet.description,
+        availability_status: newPet.availability_status,
+        image: newPet.image,
+        shelter_id: 1
       }, {
         headers: {
-          'Content-Type': 'applicaton/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
       })
       dispatch(addPet(newPet))
+      setNewPet({
+        species: '',
+        name: '',
+        breed: '',
+        birthday: '',
+        gender: '',
+        size: '',
+        description: '',
+        availability_status: '',
+        image: null,
+      });
+      alert('Pet added successfully!');
+    } catch (error) {
+      // Display error message to the user
+      alert('Error adding pet. Please try again later.');
     }
-     
-  
+  };
 
   useEffect(() => {
     fetchAdminPets()
@@ -107,83 +144,74 @@ function AdminPetList() {
 
   <div className="border d-flex flex-column">
   <h3>Add New Pet</h3>
-  <label>Species:</label>
-  <input
-    type="text"
-    name="species"
-    value={newPet.species}
-    onChange={handleInputChange}
-  />
+      <Formik
+        initialValues={newPet}
+        validationSchema={validationSchema}
+        onSubmit={handleAddPet}
+      >
+        <Form>
+          <label>Species:</label>
+          <Field type="text" name="species" />
+          <ErrorMessage name="species" component="div" className="error" />
 
-  <label>Name:</label>
-  <input
-    type="text"
-    name="name"
-    value={newPet.name}
-    onChange={handleInputChange}
-  />
+        
+          <label>Name:</label>
+          <Field type="text" name="name" />
+          <ErrorMessage name="name" component="div" className="error" />
 
-  <label>Breed:</label>
-  <input
-    type="text"
-    name="breed"
-    value={newPet.breed}
-    onChange={handleInputChange}
-  />
+        
+          <label>Breed:</label>
+          <Field type="text" name="breed" />
+          <ErrorMessage name="breed" component="div" className="error" />
 
-  <label>Birthday:</label>
-  <input
-    type="text"
-    name="birthday"
-    value={newPet.birthday}
-    onChange={handleInputChange}
-  />
+          
+          <label>Birthday:</label>
+          <Field type="text" name="birthday" />
+          <ErrorMessage name="birthday" component="div" className="error" />
 
-  <label>Gender:</label>
-  <input
-    type="text"
-    name="gender"
-    value={newPet.gender}
-    onChange={handleInputChange}
-  />
+       
+          <label>Gender:</label>
+          <Field type="text" name="gender" />
+          <ErrorMessage name="gender" component="div" className="error" />
 
-  <label>Size:</label>
-  <input
-    type="text"
-    name="size"
-    value={newPet.size}
-    onChange={handleInputChange}
-  />
+       
+          <label>Size:</label>
+          <Field type="text" name="size" />
+          <ErrorMessage name="size" component="div" className="error" />
 
-  <label>Description:</label>
-  <input
-    type="text"
-    name="description"
-    value={newPet.description}
-    onChange={handleInputChange}
-  />
+          
+          <label>Description:</label>
+          <Field type="text" name="description" />
+          <ErrorMessage name="description" component="div" className="error" />
 
-  <label>Availability Status:</label>
-  <input
-    type="text"
-    name="availability_status"
-    value={newPet.availability_status}
-    onChange={handleInputChange}
-  />
+         
+          <label>Availability Status:</label>
+          <Field type="text" name="availability_status" />
+          <ErrorMessage
+            name="availability_status"
+            component="div"
+            className="error"
+          />
 
-<label>Image:</label>
-<input
-  type="file"
-  accept="image/*"
-  onChange={handleInputChange}
-/>
+          
+          <label>Image:</label>
+          <input
+            type="file"
+            accept="image/*"
+            name="image"
+            onChange={handleInputChange}
+          />
+          <ErrorMessage name="image" component="div" className="error" />
 
-
-  <button onClick={handleAddPet}>Add Pet</button>
-</div>
+         
+          <button type="submit">Add Pet</button>
+        </Form>
+      </Formik>
+    </div>
     </AdminContainer>
 
 </main>
   </>
 }
+
 export default AdminPetList
